008e786c78ce81295c9be3a6cb2068e50c32a653
Naum <aznaum@gmail.com>
2012-02-26 16:09:54 -0700

    exodus

--- js/katiespent.js
+++ js/katiespent.js
@@ -3,7 +3,7 @@ var League = Backbone.Model.extend({
 
 $(document).ready(function() {
     var hout = '';
-    for (var i = 0; i < 9; i += 1) {
+    for (var i = 0; i < 13; i += 1) {
         var m = UBA.Man.spawn();
         hout += '<tr><td class="celtex">' +
             m.name +
@@ -11,5 +11,5 @@ $(document).ready(function() {
             UBA.Man.displaySkillCard(m) +
             '</tr>';
     }
-    $('#pagecanvas').html('<table>' + hout + '</table>');
+    $('#pagecanvas').html('<table class="cardchart">' + hout + '</table>');
 });
--- js/uba.js
+++ js/uba.js
@@ -7,15 +7,24 @@ var UBA = {
         seasonnum: 0
     },
 
+    genesis: function() {
+    },
+
     Man: {
+        ATTRCHART: {
+            'A': [ 'y', '', 'V', '~', '~~' ],
+            'D': [ 'z', '', 'I', '>', '>>' ],
+            'H': [ 'd', '', 'L', '=', '==' ],
+            'O': [ 'b', '', 'H', '$', '$$' ]
+        },
         SKILLCHART: [ 'W', 'K', 'P', 'R', 'G', 'T' ],
         SKILLMARK: {
-            'W': [ 'w', '', 'C', '^' ],
-            'K': [ 'm', '', 'K', '#' ],
-            'P': [ 'n', '', 'P', '!' ],
-            'R': [ 's', '', 'R', '+' ],
-            'G': [ 'e', '', 'G', '&' ],
-            'T': [ 'x', '', 'T', '@' ]
+            'W': [ 'w', '', 'C', '^', '^^' ],
+            'K': [ 'm', '', 'K', '#', '##' ],
+            'P': [ 'n', '', 'P', '!', '!!' ],
+            'R': [ 's', '', 'R', '+', '++' ],
+            'G': [ 'e', '', 'G', '&', '&&' ],
+            'T': [ 'x', '', 'T', '@', '@@' ]
         },
         displaySkillCard: function(m) {
             var dout = '';
@@ -26,15 +35,17 @@ var UBA = {
         },
         randSkill: function() {
             var sk = null;
-            var x = Math.floor(Math.random() * 27) + 1;
-            if (x <= 8) {
+            var x = Math.floor(Math.random() * 432) + 1;
+            if (x <= 125) {
                 sk = 0;
-            } else if (x <= 20) {
+            } else if (x <= 325) {
                 sk = 1;
-            } else if (x <= 26) {
+            } else if (x <= 415) {
                 sk = 2;
-            } else if (x <= 27) {
+            } else if (x <= 431) {
                 sk = 3;
+            } else if (x <= 432) {
+                sk = 4;
             }
             return sk;
         },
