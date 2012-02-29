79ab014db2aec16824820b879f9f09d58039af04
Naum Trifanoff <aznaum@gmail.com>
2012-02-29 15:21:43 -0700

    more css tinkering

--- js/katiespent.js
+++ js/katiespent.js
@@ -3,6 +3,10 @@ var League = Backbone.Model.extend({
 
 $(document).ready(function() {
     var hout = '';
+    hout += '<tr><th class="celtex">POS</th>' +
+        '<th class="celtex">NAME</th>' +
+        '<th class="celtex">ATTR</th>' +
+        '<th class="celtex">SKILL</th>' + '</tr>';
     for (var i = 0; i < 13; i += 1) {
         var m = UBA.Man.spawn('I');
         hout += '<tr><td class="celtex">' +
