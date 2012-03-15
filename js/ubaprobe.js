35cfb54e2de7db3a8612e648b66848aad73b20a6
Naum Trifanoff <aznaum@gmail.com>
2012-03-15 11:31:48 -0700

    adding mean skill test

--- js/ubaprobe.js
+++ js/ubaprobe.js
@@ -11,4 +11,22 @@ $(document).ready(function() {
             "total freeagents: " + UBA.data.freeagents.length);
     });
 
+    var meanskill = calcMeanSkill();
+    test("Mean skill within bounds", function() {
+        ok(meanskill > 0.95 && meanskill < 1.05,
+            'mean skill: ' + meanskill + ', not within 5%'); 
+    });
+
 });
+
+var calcMeanSkill = function() {
+    var skcount = 0, sktotal = 0;
+    $.each(UBA.data.freeagents, function(i, fa) {
+        $.each(fa.skill, function(sk, skrank) {
+            skcount += 1;
+            sktotal += skrank;
+        });
+    });
+    console.log('skcount: ' + skcount + ', sktotal: ' + sktotal);
+    return (sktotal / skcount);
+};
