9a4c5a202b6fb89a1d40cd42f479864802fab822
Naum <aznaum@gmail.com>
2012-03-15 21:33:00 -0700

    added attribute tests

--- js/ubaprobe.js
+++ js/ubaprobe.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
     test("UBA initialized", function() {
-        ok(UBA, "no UBA defined");
+        ok(UBA, "UBA exists");
     });
 
     UBA.genesis();
@@ -13,11 +13,40 @@ $(document).ready(function() {
 
     var meanskill = calcMeanSkill();
     test("Mean skill within bounds", function() {
-        ok(meanskill > 0.95 && meanskill < 1.05,
-            'mean skill: ' + meanskill + ', not within 5%'); 
+        ok(meanskill > 0.95 && meanskill < 1.05, 'mean skill: ' + meanskill); 
+    });
+
+    var meanattr = calcMeanAttr();
+    test("Mean attribute within bounds", function() {
+        ok(meanattr > 0.95 && meanattr < 1.05, 'mean attr: ' + meanattr);
+    });
+
+    var meanage = calcMeanAge();
+    test("Mean age within bounds", function() {
+        ok(meanage > 0.9166 && meanage < 1.0833, 'mean age: ' + meanage);
     });
 
 });
+
+var calcMeanAge = function() {
+    var attrcount = 0, agetotal = 0;
+    $.each(UBA.data.freeagents, function(i, fa) {
+        attrcount += 1;
+        agetotal += fa.attr.A;
+    });
+    return (agetotal / attrcount);
+};
+
+var calcMeanAttr = function() {
+    var attrcount = 0, attrtotal = 0;
+    $.each(UBA.data.freeagents, function(i, fa) {
+        $.each(fa.attr, function(a, attrmark) {
+            attrcount += 1;
+            attrtotal += attrmark;
+        });
+    });
+    return (attrtotal / attrcount);
+};
 
 var calcMeanSkill = function() {
     var skcount = 0, sktotal = 0;
@@ -27,6 +56,5 @@ var calcMeanSkill = function() {
             sktotal += skrank;
         });
     });
-    console.log('skcount: ' + skcount + ', sktotal: ' + sktotal);
     return (sktotal / skcount);
 };
